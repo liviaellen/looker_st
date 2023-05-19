ef2a2df8c6d657120c64eb7c0aada2087b8f1fd8
liviaellen <38367623+liviaellen@users.noreply.github.com>
2023-05-19 19:43:01 +0800

    Update smart_ellen.py

--- smart_ellen.py
+++ smart_ellen.py
@@ -10,19 +10,23 @@ text_input = st.text_input('Ask me anything', "Write a paragraph about a guy nam
 
 if st.button("Ask"):
     openai.api_key=st.secrets["API_KEY"]
-    response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=text_input,
-    temperature=1,
-    max_tokens=256,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-    )
-    if response['choices'][0].text == "":
+    try:
+        response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=text_input,
+        temperature=1,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+        )
+        if response['choices'][0].text == "":
 
-        st.write("Sorry, I don't know that one.")
-        st.write("Error message: ")
-        st.write(response)
-    else:
-        st.write(response['choices'][0].text)
+            st.write("Sorry, I don't know that one.")
+            st.write("Error message: ")
+            st.write(response)
+        else:
+            st.write(response['choices'][0].text)
+     except Exception as e:
+        st.write(e.message)
+        st.write(e.args)

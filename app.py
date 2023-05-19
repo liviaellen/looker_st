06875ba0ce3830b6aa09ee9fb83e973b0603bca7
Livia Ellen <--liviaellen@msn.com>
2023-05-19 17:06:20 +0800

    init push

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.set_page_config(layout="wide")
+# st.markdown("""
+#     <iframe width="600" height="606" src="https://datastudio.google.com/embed/reporting/1uYIXuUFbGwMA8Y8R2JFenPNG3fjuCaGG/page/mpeW" frameborder="0" style="border:0" allowfullscreen></iframe>
+#     """, unsafe_allow_html=True)
+
+"""# Greenweez Dashboard """
+
+st.markdown("""
+            <iframe width="1200" height="800" src="https://lookerstudio.google.com/embed/reporting/1979528f-a44b-4f6e-a762-e449b8beef06/page/bAP6C" frameborder="0" style="border:20" allowfullscreen></iframe>
+            """, unsafe_allow_html=True)

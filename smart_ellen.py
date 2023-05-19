c21a8ff1e2ec258c8a8d6c82271ef7d4c0430b47
liviaellen <38367623+liviaellen@users.noreply.github.com>
2023-05-19 18:07:36 +0800

    Update smart_ellen.py

--- smart_ellen.py
+++ smart_ellen.py
@@ -1,19 +1,27 @@
 import streamlit as st
 import openai
 
-"""# Smart Ellen "
+"""# Smart Ellen """
 text_input = st.text_input('Ask me anything', "Write a paragraph about a guy named mike who moves to bali to study data analytics in a coding bootcamp who just celebrate his birthday")
 
 # openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.api_key ="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"
-response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt=text_input,
-  temperature=1,
-  max_tokens=256,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
-)
 
-st.write(response['choices'][0].text)
+
+if st.button("Ask"):
+    openai.api_key ="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"
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=text_input,
+    temperature=1,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    if response['choices'][0].text == "":
+
+        st.write("Sorry, I don't know that one.")
+        st.write("Error message: ")
+        st.write(response)
+    else:
+        st.write(response['choices'][0].text)
